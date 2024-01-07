cbd555a0da60635129c9a2ad6fec227883b790c3
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2024-01-06 23:38:18 -0600

    slight change in shape URLS

--- scripts/scrapeIconsAndShapes.js
+++ scripts/scrapeIconsAndShapes.js
@@ -10,7 +10,7 @@ for (let i = 0; i < agencies.length; i++) {
   const agency = agencies[i];
 
   const icons = fs.readdirSync(`./data/${agency}/icons`).map((fileName) => `https://gtfs.piemadd.com/data/${agency}/icons/${fileName}`);
-  const shapes = fs.readdirSync(`./data/${agency}/shapes`).map((fileName) => `https://gtfs.piemadd.com/data/${agency}/shapes/${fileName}`);
+  const shapes = fs.readdirSync(`./data/${agency}/shapes`).map((fileName) => `${agency}/shapes/${fileName}`);
 
   const meta = JSON.parse(fs.readFileSync(`./data/${agency}/meta.json`, { encoding: 'utf8' }));
 
