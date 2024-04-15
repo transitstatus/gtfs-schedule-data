21d423ea33993e9abd8a841e8d32292af13f65f4
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2024-04-15 17:30:56 -0500

    made sure cleanup only happens on a server instance

--- scripts/cleanup.js
+++ scripts/cleanup.js
@@ -2,4 +2,4 @@ const fs = require('fs');
 
 if (fs.existsSync('./csv')) fs.rmSync('./csv', { recursive: true });
 if (fs.existsSync('./zips')) fs.rmSync('./zips', { recursive: true });
-if (fs.existsSync('./node_modules')) fs.rmSync('./node_modules', { recursive: true });
+//if (fs.existsSync('./node_modules')) fs.rmSync('./node_modules', { recursive: true });
