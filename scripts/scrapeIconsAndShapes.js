46f9ba07509db61da2599e4f4f9ed452acc56642
piemadd <piero@piemadd.com>
2025-10-30 15:34:29 -0500

    oops

--- scripts/scrapeIconsAndShapes.js
+++ scripts/scrapeIconsAndShapes.js
@@ -2,24 +2,20 @@ const fs = require('fs');
 
 const agencies = fs.readdirSync('./data');
 
-let iconsList = [];
 let shapesList = [];
 let colorsList = [];
 
 for (let i = 0; i < agencies.length; i++) {
   const agency = agencies[i];
 
-  const icons = fs.readdirSync(`./data/${agency}/icons`).map((fileName) => `https://gtfs.piemadd.com/data/${agency}/icons/${fileName}`);
   const shapes = fs.readdirSync(`./data/${agency}/shapes`).map((fileName) => `${agency}/shapes/${fileName}`);
 
   const meta = JSON.parse(fs.readFileSync(`./data/${agency}/meta.json`, { encoding: 'utf8' }));
 
   colorsList.push(...meta.colorSets);
 
-  iconsList.push(...icons);
   shapesList.push(...shapes);
 }
 
-fs.writeFileSync('./iconsList.json', JSON.stringify(iconsList));
 fs.writeFileSync('./shapesList.json', JSON.stringify(shapesList));
 fs.writeFileSync('./colorsList.json', JSON.stringify([...new Set(colorsList.sort())]));
